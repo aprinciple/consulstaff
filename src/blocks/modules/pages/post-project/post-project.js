d8faad28f666a4695af7a2dd47a3a43d219ec146
Alexander Principle <avprinciple@gmail.com>
2020-02-29 18:16:01 +0200

    Added scroll-behaviour smooth

---
+++ src/blocks/modules/pages/post-project/post-project.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const pagePostProject = document.querySelector('.page-post-project');
+  
+  if (pagePostProject) {
+    const htmlEl = document.documentElement;
+    htmlEl.style.scrollBehavior = 'smooth';
+  }
+});
