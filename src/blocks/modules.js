9723c6c5165a149c54314b48339da8f381d3a16d
Alexander Principle <avprinciple@gmail.com>
2020-04-24 01:10:24 +0300

    Removed hack for viewport unit mobile

--- src/blocks/modules.js
+++ src/blocks/modules.js
@@ -1,9 +1,5 @@
 /* eslint-disable */
 
-// Fix viewport units for height
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 // Common
 import '../blocks/modules/common/menu/menu';
 import '../blocks/modules/common/slider-promo/slider-promo';
