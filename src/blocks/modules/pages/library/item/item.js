8335ab52ff984969b8d9b6de972a436ce058c061
aprinciple <avprinciple@gmail.com>
2021-02-28 16:56:05 +0200

    Added v.2.0

--- src/blocks/modules/pages/library/item/item.js
+++ src/blocks/modules/pages/library/item/item.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const libraryItems = document.querySelector('.page-library__items');
+
   if(libraryItems) {
     const options = {
       root: null,
-      rootMargin: '0px 0px -40% 0px',
-      threshold: 1.0
+      rootMargin: '0px 0px -70% 0px',
+      threshold: 0
     }
     
     const callback = function(entries, observer) { 
       entries.forEach(entry => {
           if(entry.isIntersecting) {
             entry.target.classList.add('open');
-            observer.unobserve(entry.target);
+          } else {
+            entry.target.classList.remove('open');
           }
       });
     };
